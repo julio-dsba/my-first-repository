ed6ecf4112e2e8eac97bd5aa11f0cd62b13a02e9
julio <julio.dsba@gmail.com>
2021-01-21 23:05:05 +0300

    A push in the second branch!

--- workshop4/main.cpp
+++ workshop4/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+void branch2()
+{
+    std :: cout << "I am coding this in the second branch!" << std :: endl;
+}
+
 int main()
 {
     std :: cout << "Hello World Once More!" << std :: endl;
